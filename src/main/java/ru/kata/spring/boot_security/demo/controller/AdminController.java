a03fd1fd6fee8fa92cf390d5593315671b9357a4
silvestrr <nikitamaliev000@gmail.com>
2022-08-03 17:35:56 +0300

    new

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,15 +1,61 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+
     private final UserService userService;
 
+    @Autowired
     public AdminController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping()
+    public String getAllUser(Model model) {
+        model.addAttribute("users", userService.index());
+        return "users";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}")
+    public String getUser(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.showUserById(id));
+        return "user";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editUser(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.showUserById(id));
+        return "edit";
+    }
+
+    @PatchMapping("{id}")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.update(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        userService.delete(id);
+        return "redirect:/admin";
     }
 }
