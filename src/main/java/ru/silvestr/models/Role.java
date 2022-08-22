9a6b82fef9d3e681a17e6473655418c16d1ecb71
silvestrr <nikitamaliev000@gmail.com>
2022-08-22 18:35:04 +0300

    new

--- src/main/java/ru/silvestr/models/Role.java
+++ src/main/java/ru/silvestr/models/Role.java
@@ -2,6 +2,7 @@ package ru.silvestr.models;
 
 import org.springframework.security.core.GrantedAuthority;
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "role")
@@ -43,5 +44,26 @@ public class Role implements GrantedAuthority {
     @Override
     public String getAuthority() {
         return getRole();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role1 = (Role) o;
+        return Objects.equals(id, role1.id) && Objects.equals(role, role1.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, role);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                '}';
     }
 }
