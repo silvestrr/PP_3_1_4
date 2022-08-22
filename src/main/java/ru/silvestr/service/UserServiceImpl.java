ab4f67b9f814bf8e5437cef4f395b67e874451a0
silvestrr <nikitamaliev000@gmail.com>
2022-08-22 20:13:20 +0300

    new

--- src/main/java/ru/silvestr/service/UserServiceImpl.java
+++ src/main/java/ru/silvestr/service/UserServiceImpl.java
@@ -76,8 +76,8 @@ public class UserServiceImpl implements UserService {
         Set<Role> roles2 = new HashSet<>();
         roles2.add(roleRepository.findById(1L).orElse(null));
         roles2.add(roleRepository.findById(2L).orElse(null));
-        User user1 = new User("User","Userov",(byte) 25, "user@mail.com", "user","user",roles1);
-        User user2 = new User("Admin","Adminov",(byte) 30, "admin@mail.com", "admin","admin",roles2);
+        User user1 = new User("User","Userov",(byte) 20, "user@mail.com", "user","user",roles1);
+        User user2 = new User("Admin","Adminov",(byte) 21, "admin@mail.com", "admin","admin",roles2);
         save(user1);
         save(user2);
         }
